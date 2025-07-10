28aae259ac9c566a21a5716ad920be6ac9a7086c
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-07-10 14:01:36 -0400

    One Variable > 216
    The way I put in the text does not make sense for args.

--- revised_syc_formal.py
+++ revised_syc_formal.py
@@ -105,7 +105,9 @@ if prompt := st.chat_input("Ask anything"):
     response = chain_with_history.invoke({"query": prompt}, config)
     st.chat_message("Assistant").write(response.content)
 
-    st.button("Copy to Clipboard 📋", on_click=on_copy_click, args=("User: " + prompt + "\n Assistant: " + response.content + "\n"))
+    text = "User: " + prompt + "\n Assistant: " + response.content + "\n"
+
+    st.button("Copy to Clipboard 📋", on_click=on_copy_click, args=(text))
 
 # Acess the html for the streamlit GUI w/ IFrame
 components.html(
