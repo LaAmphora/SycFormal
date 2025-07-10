d4cd627ecf193e67a0a95db01ff641dadb5bd1e9
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-07-10 16:57:51 -0400

    Make Markdown
    If centering + markdown looks better?

--- revised_syc_formal.py
+++ revised_syc_formal.py
@@ -133,7 +133,7 @@ if msgs.messages:
         # Button configured w/ html to copy to clipboard
         st.button("Copy to Clipboard 📋")
     with col2:
-        st.subheader(":blue-background[Copy conversation into the form!]")
+        st.markdown(":blue-background[Copy conversation into the form!]")
 
 
 components.html(
